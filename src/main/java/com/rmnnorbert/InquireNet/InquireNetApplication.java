e1108b9ab8a7c45d9942e657ed84fa548dba989a
Norbert Remenyi <remenyinorbert@gmail.com>
2023-06-08 12:14:48 +0200

    Initial commit: Create basic application structure

---
+++ src/main/java/com/rmnnorbert/InquireNet/InquireNetApplication.java
@@ -0,0 +1,13 @@
+package com.rmnnorbert.InquireNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquireNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquireNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmnnorbert/InquireNet/InquireNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmnnorbert.InquireNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquireNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

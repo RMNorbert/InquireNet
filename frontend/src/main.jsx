542afba3fa1ff7c6c69da66fbf0d25ea1048b882
Norbert Remenyi <remenyinorbert@gmail.com>
2023-06-11 17:51:14 +0200

    Feat: add QuestionList Chore: add BrowserRouter to main.jsx with basic paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Register } from "./components/user/Register";
+import { Login } from "./components/user/Login";
+import { SelectedQuestion } from "./components/questions/SelectedQuestion";
+import { CreateQuestion } from "./components/questions/CreateQuestion";
+import { Header } from "./components/Header";
+import { User } from "./components/user/User";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/question",
+        element: <App />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/question/:id",
+        element: <SelectedQuestion />,
+    },
+    {
+        path: "/createquestion",
+        element: <CreateQuestion />,
+    },
+    {
+        path: "/user",
+        element: <User />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <div>
+            <Header />
+            <RouterProvider router={router} />
+        </div>
+    </React.StrictMode>
+);
+

097510aef7c3634f7d174c27783e46fd7cf7e068
Norbert Remenyi <remenyinorbert@gmail.com>
2023-10-11 20:06:57 +0200

    feat: add sql file for insert initial chat data

---
+++ src/main/resources/db/migration/V1.1/V1.1.7__Insert_Initial_Chat_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO chat (user_id, title, role, content)
+VALUES (1, 'Chat 1', 'admin', 'Content of Chat 1'),
+       (2, 'Chat 2', 'user', 'Content of Chat 2'),
+       (1, 'Chat 3', 'user', 'Content of Chat 3');

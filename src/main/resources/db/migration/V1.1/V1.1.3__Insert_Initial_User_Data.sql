389f7a02adfa3143b1903d05e4b6ea9fd5835060
Norbert Remenyi <remenyinorbert@gmail.com>
2023-10-11 20:06:19 +0200

    feat: add sql file for insert initial user data

---
+++ src/main/resources/db/migration/V1.1/V1.1.3__Insert_Initial_User_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" (role, username, password, registration_date)
+VALUES ('admin', 'admin_user', 'admin_password', '2023-10-11 10:00:00'),
+       ('user', 'regular_user', 'user_password', '2023-10-11 11:00:00'),
+       ('user', 'another_user', 'another_password', '2023-10-11 12:00:00');

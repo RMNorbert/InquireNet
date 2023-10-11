b19aa7a6228f2f3b718a0b7bb5c324c23de968ac
Norbert Remenyi <remenyinorbert@gmail.com>
2023-10-11 20:06:28 +0200

    feat: add sql file for insert initial question data

---
+++ src/main/resources/db/migration/V1.1/V1.1.4__Insert_Initial_Question_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO question (user_id, title, description, created)
+VALUES (1, 'Sample Question 1', 'Description of Sample Question 1', '2023-10-11 10:00:00'),
+       (2, 'Sample Question 2', 'Description of Sample Question 2', '2023-10-11 11:00:00'),
+       (1, 'Sample Question 3', 'Description of Sample Question 3', '2023-10-11 12:00:00');

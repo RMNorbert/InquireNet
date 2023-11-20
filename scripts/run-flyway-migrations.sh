fbe3d01e0927b8cd48062ab2d6d2fb58347046ca
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-20 16:10:07 +0100

    chore: move run-flyway-migrations.sh into scripts directory

--- scripts/run-flyway-migrations.sh
+++ scripts/run-flyway-migrations.sh
@@ -2,7 +2,7 @@
 DATABASE_URL="jdbc:postgresql://localhost:5432/InquireNet"
 DATABASE_USER="postgres"
 DATABASE_PASSWORD="postgres"
-LOCATIONS="filesystem:./src/main/resources/db/"
+LOCATIONS="filesystem:../src/main/resources/db/"
 FLYWAY_CMD="flyway -url=$DATABASE_URL -user=$DATABASE_USER -password=$DATABASE_PASSWORD -locations=$LOCATIONS migrate"
 
 $FLYWAY_CMD
